8b806294dc5edc583461883b0edadaf1cbaaffba
Zhouxing Shi <zhouxingshichn@gmail.com>
2018-11-11 19:56:25 +0800

    add exe_mem

---
+++ thinpad_top.srcs/sources_1/modules/exe_mem.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+
+module exe_mem(
+    input wire clk,
+    input wire[31:0] alu_res,
+    input wire alu_s,
+    input wire alu_z,
+    input wire[31:0] inst_in,
+    input wire[4:0] reg_write_address_in,
+    input wire[31:0] mem_write_data_in,
+    input wire[31:0] pc_plus_4,
+    input wire[31:0] data_A, 
+    input wire[31:0] pc_jump_in,
+
+    // control signals
+    input wire[1:0] con_reg_data,
+    input wire con_branch, 
+    input wire con_branch_rev, // ne
+    input wire con_branch_s, // s or z
+    input wire con_jump,
+
+    output reg[31:0] inst_out,
+    output reg[31:0] mem_address,
+    output reg[31:0] mem_write_data,
+    output reg[4:0] reg_write_address,
+    output reg[31:0] reg_write_data,
+    output reg[31:0] alu_z_out,
+    output reg[31:0] pc_jump,
+    output reg pc_src
+);
+
+// whether should branch if it is a branch instruction
+wire alu_b;
+assign alu_b = con_branch_s ? alu_s : (alu_z ^ con_branch_rev);
+
+always @(posedge clk) begin
+    inst_out <= inst_in;
+    mem_address <= alu_res;
+    mem_write_data <= mem_write_data_in;
+    reg_write_address <= reg_write_address_in;
+    alu_z_out <= alu_z;
+    pc_jump <= pc_jump_in;
+
+    if (con_reg_data[1]) 
+        reg_write_data <= pc_plus_4;
+    else
+        // data_A is for mov 
+        reg_write_data <= con_reg_data[0] ? alu_res : data_A; 
+
+    // should branch or jump?
+    pc_src <= (alu_b & con_branch) | con_jump;
+end
+
+endmodule
--- thinpad_top.srcs/sources_1/modules/id_exe.v
+++ thinpad_top.srcs/sources_1/modules/id_exe.v
@@ -8,15 +8,16 @@ module id_exe(
     input wire[31:0] pc_plus_4_in,
 
     // control signals
-    input wire alu_immediate,
-    input wire[1:0] reg_dst,
+    input wire con_alu_immediate,
+    input wire[1:0] con_reg_dst,
 
     output reg[31:0] inst_out,
+    output reg[31:0] pc_plus_4_out,
     output reg[31:0] pc_jump,
     output reg[31:0] data_A, // for alu
     output reg[31:0] data_B, // for alu
-    output reg[5:0] reg_write_address,
-    output reg[31:0] mem_write_address
+    output reg[4:0] reg_write_address,
+    output reg[31:0] mem_write_data
 ); 
 
 wire immediate_16, immediate_sign_extend;
@@ -25,16 +26,17 @@ assign immediate_sign_extend = { immediate_16[15] ? 16'hffff : 16'h0, immediate_
 
 always @(posedge clk) begin
     inst_out <= inst_in;
+    pc_plus_4_out <= pc_plus_4_in;
     pc_jump <= pc_plus_4_in + (immediate_sign_extend << 2'b10);
     data_A <= data_1;
-    data_B <= alu_immediate ? immediate_sign_extend : data_2;
+    data_B <= con_alu_immediate ? immediate_sign_extend : data_2;
 
-    if (reg_dst[1])
-        reg_write_address = 5'b10000; // save $31 for jal
+    if (con_reg_dst[1])
+        reg_write_address <= 5'b10000; // save $31 for jal
     else
-        reg_write_address <= reg_dst[0] ? inst_in[20:16] : inst_in[15:11];
+        reg_write_address <= con_reg_dst[0] ? inst_in[20:16] : inst_in[15:11];
 
-    mem_write_address <= data_1;
+    mem_write_data <= data_2;
 end
 
 endmodule
